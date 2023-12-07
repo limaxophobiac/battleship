88b8d8223759d7627326cedecf42bb1a2611f235
limax <joel_bolkert@hotmail.com>
2023-12-07 17:52:22 +0100

    trying to remember webpack stuff

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**************************!*\
+  !*** ./src/gamelogic.js ***!
+  \**************************/
+function testfunc(){
+    console.log("tested")
+}
+
+let k  = 323
+console.log("hello")
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQTs7QUFFQTtBQUNBLG9CIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VicGFjay1kZW1vLy4vc3JjL2dhbWVsb2dpYy5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJmdW5jdGlvbiB0ZXN0ZnVuYygpe1xuICAgIGNvbnNvbGUubG9nKFwidGVzdGVkXCIpXG59XG5cbmxldCBrICA9IDMyM1xuY29uc29sZS5sb2coXCJoZWxsb1wiKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
